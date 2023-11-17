4d7d4effee2bbcb4ef31d0fdb40fefd864f8c4ab
Plak1n <ms.joker590@gmail.com>
2023-11-17 14:31:37 +0300

    last

--- courses/models.py
+++ courses/models.py
@@ -38,7 +38,7 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
-
+    
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title) 
     #     super(Course, self).save(*args, **kwargs)
